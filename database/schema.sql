07df675587eb4dcd6761cb2029df01837ca992fe
this-is-yosuke <yosuke.kibe@gmail.com>
2024-11-10 19:02:19 -0700

    Updated the schema.sql
    fixed the foreign keys

--- database/schema.sql
+++ database/schema.sql
@@ -7,10 +7,14 @@ CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     employee_firstName VARCHAR(100) NOT NULL
     employee_lastName VARCHAR(100) NOT NULL
+    role_id INT
+    department_id INT
+    role_salary INT
+    manager_id INT
     FOREIGN KEY (role_id) REFERENCES roles(id)
     FOREIGN KEY (department_id) REFERENCES departments(id)
     FOREIGN KEY (role_salary) REFERENCES roles(salary)
-    FOREIGN KEY (employee_manager) REFERENCES managers(id)
+    FOREIGN KEY (manager_id) REFERENCES managers(id)
 );
 
 CREATE TABLE departments (
@@ -21,6 +25,7 @@ CREATE TABLE departments (
 CREATE TABLE roles (
     id SERIAL PRIMARY KEY
     role_name VARCHAR(100) NOT NULL
+    department_id INT
     FOREIGN KEY (department_id) REFERENCES departments(id)
     salary NUMBER NOT NULL
 );
@@ -28,5 +33,6 @@ CREATE TABLE roles (
 CREATE TABLE managers (
     id SERIAL PRIMARY KEY
     manager_fullName VARCHAR(100) NOT NULL
+    department_id INT
     FOREIGN KEY (department_id) REFERENCES departments(id)
 );
