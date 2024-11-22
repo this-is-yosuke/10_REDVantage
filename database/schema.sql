f5484b5e86922eb419eed7a6d2c8f438ba1e93e7
this-is-yosuke <yosuke.kibe@gmail.com>
2024-11-21 23:56:26 -0700

    Updated schema.sql
    Rearranged table order. Added missing commas.

--- database/schema.sql
+++ database/schema.sql
@@ -3,25 +3,25 @@ CREATE DATABASE redvantage_db;
 
 \c redvantage_db
 
-CREATE TABLE employees (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL
-    last_name VARCHAR(100) NOT NULL
-    role_id INT
-    manager_id INT
-    FOREIGN KEY (role_id) REFERENCES roles(id)
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
-);
-
 CREATE TABLE departments (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
     department_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE roles (
-    id SERIAL PRIMARY KEY
-    title VARCHAR(100) NOT NULL
-    salary NUMBER NOT NULL
-    department INT
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    salary INTEGER NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
