32807c095b0e7ee0846f6bccdad1641c80f63a54
this-is-yosuke <yosuke.kibe@gmail.com>
2024-11-22 01:05:16 -0700

    Updated seeds.sql
    Reordered the insert sequence.

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,17 +1,8 @@
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('john', 'doe', 1, null),
-       ('jane', 'doe', 5, null),
-       ('george', 'washington', 4, null),
-       ('joe', 'schmoe', 6, 2),
-       ('jean-luc', 'picard', 7, null),
-       ('jim', 'schmim', 8, 5),
-       ('saul', 'goodman', 2, 1);
-
 INSERT INTO departments (department_name)
 VALUES ('sales'),
        ('engineering'),
        ('finance'),
-       ('legal')
+       ('legal');
 
 INSERT INTO roles (title, salary, department)
 VALUES ('sales lead', 110000, 1),
@@ -21,4 +12,13 @@ VALUES ('sales lead', 110000, 1),
        ('account manager', 75000, 3),
        ('accountant', 50000, 3),
        ('legal team lead', 65000, 4),
-       ('lawyer', 60000, 4)
+       ('lawyer', 60000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('john', 'doe', 1, null),
+       ('jane', 'doe', 5, null),
+       ('george', 'washington', 4, null),
+       ('joe', 'schmoe', 6, 2),
+       ('jean-luc', 'picard', 7, null),
+       ('jim', 'schmim', 8, 5),
+       ('saul', 'goodman', 2, 1);
