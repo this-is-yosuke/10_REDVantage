829484000bd6a19f2adb35cfb796b5b110247225
this-is-yosuke <yosuke.kibe@gmail.com>
2024-11-21 23:58:33 -0700

    Updated server.ts
    Added the Inquirer. No idea if it works yet.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { QueryResult } from 'pg';
-import { pool, connectToDB } from './connection';
+import { pool, connectToDB } from './connection.js';
+import inquirer from 'inquirer';
 
 await connectToDB();
 
@@ -10,3 +11,32 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// Using inquirer
+inquirer.prompt([
+    {
+        type: "input",
+        name: "test",
+        message: "Testing to see if this works",
+    }])
+    .then((response) => {
+        console.log(`${response.test}`);
+        pool.query('SELECT * FROM employees', (err: Error, result: QueryResult) => {
+            if(err){
+                console.log(err);
+            }else if (result) {
+                console.log(result.rows);
+            }
+        })
+    });
+
+    // Default error message
+    app.use((_req, res) => {
+        res.status(404).end();
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+
+    
