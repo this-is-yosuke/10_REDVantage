31c54292df88220ff4e3c44f4a7a3df6c98c2a29
this-is-yosuke <yosuke.kibe@gmail.com>
2024-11-23 02:36:19 -0700

    Updated server.ts
    Now that I know the Inquirer works, I replaced the test with the desired SELECT queries. Added an exit. Needs the INSERT queries and UPDATE query still.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import { QueryResult } from 'pg';
+import express, { application } from 'express';
+import { Client, Connection, Pool, QueryResult } from 'pg';
 import { pool, connectToDB } from './connection.js';
 import inquirer from 'inquirer';
 
@@ -13,22 +13,67 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 // Using inquirer
-inquirer.prompt([
-    {
-        type: "input",
-        name: "test",
-        message: "Testing to see if this works",
-    }])
-    .then((response) => {
-        console.log(`${response.test}`);
-        pool.query('SELECT * FROM employees', (err: Error, result: QueryResult) => {
-            if(err){
-                console.log(err);
-            }else if (result) {
-                console.log(result.rows);
+function startCli(): void {
+
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "dbQuery",
+            message: "Pick your poison!",
+            choices: ['Select all departments', 'Select all roles', 'Select all employees', 'Add a department',
+                'Add a role', 'Add an employee', 'Update an employee', 'Exit'],
+        }])
+        .then((response) => {
+            console.log(`${response.dbQuery}`);
+            switch (response.dbQuery) {
+                case 'Select all departments':
+                    pool.query('SELECT * FROM departments', (err: Error, result: QueryResult) => {
+                        if(err){
+                            console.log(err);
+                        }else if(result) {
+                            console.log(result.rows);
+                        }
+                        startCli();
+                    });
+                    break;
+                    // startCli();
+                case 'Select all roles':
+                    pool.query('SELECT * FROM roles', (err: Error, result: QueryResult) => {
+                        if(err){
+                            console.log(err);
+                        }else if(result) {
+                            console.log(result.rows);
+                        }
+                        startCli();
+                    });
+                    break;
+                case 'Select all employees':
+                    pool.query('SELECT * FROM employees', (err: Error, result: QueryResult) => {
+                        if(err){
+                            console.log(err);
+                        }else if (result) {
+                            console.log(result.rows);
+                        }
+                        startCli();
+                    })
+                    break;
+                case 'Add a department':
+                    break;
+                case 'Add a role':
+                    break;
+                case 'Add an employee':
+                    break;
+                case 'Update an employee':
+                    break;
+                default:
+                    console.log("Have a nice day!");
+                //    process.abort(); works, BUT generates a stack trace in the console
+                    // process.disconnect(); not a function?
+                    process.exit();
             }
-        })
-    });
+        });
+};
+startCli();
 
     // Default error message
     app.use((_req, res) => {
